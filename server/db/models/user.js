22b507bad4110f5e362b4a413a73da2491b95f42
ccnixon <chrisnxn@gmail.com>
2015-11-13 16:59:38 -0800

    [feat]adding langauges to new user

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -137,17 +137,12 @@ User.create = function(username){
 
 //pass in a username and callback the callback will act on the new User object 
 
-User.findRecommendations = function(user){
-  
-}
-
 User.get = function(userName, cb){  
   db.find({username: userName}, 'USER', function(err, person){
     var user = new User(person);
     cb(user._node[0]);
   })
 }
-
 
 
 

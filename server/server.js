1f8e23d024d64e4f7eb632b8c752079e772631ce
Yusuf Modan <yumodan@gmail.com>
2015-11-17 13:41:04 -0800

    fix: fix directory path for sql models

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var cookieParser = require('cookie-parser');
 var http = require('http');
 var sockets = require('socket.io');
 var User = require('./db/models/user.js').User;
-var sql = require('./db/models/sqlModels/sqlModels.js');
+var sql = require('./db/models/sqlModels.js');
 
 var app = express();
 

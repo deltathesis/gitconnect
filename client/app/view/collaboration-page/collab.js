7990f998f0a12e02ba4fc205d9d5ab205a24c81b
ccnixon <chrisnxn@gmail.com>
2015-11-18 11:33:08 -0800

    [bug] fixed issue with authentication trying to create a new user on login

--- client/app/view/collaboration-page/collab.js
+++ client/app/view/collaboration-page/collab.js
@@ -10,4 +10,11 @@ angular.module('myApp.collaboration-page', ['ngRoute'])
 
 .controller('collaboration-page', ['$scope', function($scope) {
 
+  $scope.projectInfo = {
+    name: 'GitConnect',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    tech: ['HTML5', 'JavaScript', 'Firebase', 'MySql'],
+    github_url: 'https://github.com/deltathesis/gitconnect'
+    }
+
 }]);

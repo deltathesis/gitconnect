d4cbb3d9dfd4102ec76d83c74fd1a9ef34fef27c
Renan Deswarte <renandeswarte@gmail.com>
2015-11-17 16:16:59 -0800

    feat: add profile page template and view

--- client/app/view/profile-page/profile-page.js
+++ client/app/view/profile-page/profile-page.js
@@ -10,4 +10,33 @@ angular.module('myApp.profilepage', ['ngRoute'])
 
 .controller('profilePage', ['$scope', function($scope) {
 
+  var user = {
+    picture: 'assets/pictures/users/royce.jpg',
+    name: 'Royce Leung',
+    memberDate: 1447797324755,
+    ratings: Math.round(4.2),
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    projects: [
+      {name:'Ballr', id:'123'},
+      {name:'GitConnect', id:'987'},
+      {name:'Foodly', id:'567'},
+      {name:'InstaCutz', id:'0987'},
+      {name:'Humus', id:'12883'}
+    ],
+    languages: ['Javascript', 'Angular', 'Sass', 'CSS', 'HTML5', 'Firebase']
+  }
+
+  $scope.user = user;
+
+  $scope.ratings = function() {
+    // Ratings Module
+    $ratings = $('.stars');
+    for (var pos = 0; pos < 5; pos++) {
+      $ratings.append("<i class='fa fa-star-o position-" + pos + "'></i>");
+    }
+    for (var i = 0; i < user.ratings; i++) {
+      $('.position-' + i).removeClass('fa-star-o').addClass('fa-star');
+    }
+  }
+
 }]);

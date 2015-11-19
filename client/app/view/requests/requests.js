32f5be00ca10ab295f4dffc2c99dab2238221ecf
Renan Deswarte <renandeswarte@gmail.com>
2015-11-18 22:44:29 -0800

    feat: requests/demands page view and animations completed

--- client/app/view/requests/requests.js
+++ client/app/view/requests/requests.js
@@ -50,11 +50,11 @@ angular.module('myApp.requests', ['ngRoute'])
 
   var usersDemand = [
     {
-      picture: 'assets/pictures/users/renan.jpg',
-      name: 'Renan Deswarte',
-      requestedDate: 1447897324755,
-      ratings: Math.round(3.2),
-      languages: ['Javascript', 'Backbone', 'Stylus', 'MongoDB', 'HTML5', 'Socket.io']
+      picture: 'assets/pictures/users/chris.jpg',
+      name: 'Chris Nixon',
+      requestedDate: 1427797324755,
+      ratings: Math.round(4.1),
+      languages: ['Javascript', 'Angular', 'Neo4J', 'CSS', 'Protractor']
     },
     {
       picture: 'assets/pictures/users/yumo.jpg',
@@ -62,6 +62,13 @@ angular.module('myApp.requests', ['ngRoute'])
       requestedDate: 1442797324755,
       ratings: Math.round(4.8),
       languages: ['Javascript', 'React', 'React Native', 'Jasmine', 'Neo4J', 'Firebase', 'Grunt']
+    },
+    {
+      picture: 'assets/pictures/users/renan.jpg',
+      name: 'Renan Deswarte',
+      requestedDate: 1447897324755,
+      ratings: Math.round(3.2),
+      languages: ['Javascript', 'Backbone', 'Stylus', 'MongoDB', 'HTML5', 'Socket.io']
     },
     {
       picture: 'assets/pictures/users/jake.jpg',
@@ -76,17 +83,22 @@ angular.module('myApp.requests', ['ngRoute'])
       requestedDate: 1447797324755,
       ratings: Math.round(4.2),
       languages: ['Javascript', 'Angular', 'Sass', 'CSS', 'HTML5', 'Firebase']
-    },
-    {
-      picture: 'assets/pictures/users/chris.jpg',
-      name: 'Chris Nixon',
-      requestedDate: 1427797324755,
-      ratings: Math.round(4.1),
-      languages: ['Javascript', 'Angular', 'Neo4J', 'CSS', 'Protractor']
     }
   ]
 
   $scope.usersRequest = usersRequest;
   $scope.usersDemand = usersDemand;
+
+  $scope.ratings = function(ratings, index, type) {
+    // Ratings Module
+    index++;
+    $ratings = $('.user-details.' + type + ':nth-child(' + index + ') .stars');
+    for (var pos = 0; pos < 5; pos++) {
+      $ratings.append("<i class='fa fa-star-o position-" + pos + "'></i>");
+    }
+    for (var i = 0; i < ratings; i++) {
+      $('.user-details.' + type + ':nth-child(' + index + ') .position-' + i).removeClass('fa-star-o').addClass('fa-star');
+    }
+  }
 
 }]);

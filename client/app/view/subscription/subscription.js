57b93b087421c563b76e206f6ffa62b77cfc3b0b
Renan Deswarte <renandeswarte@gmail.com>
2015-11-22 11:35:39 -0800

    fix: add user info from cookie in subscription page

--- client/app/view/subscription/subscription.js
+++ client/app/view/subscription/subscription.js
@@ -8,23 +8,18 @@ angular.module('myApp.subscription', ['ngRoute'])
   });
 }])
 
-.controller('subscriptionPage', ['$scope', '$location', function($scope, $location) {
+.controller('subscriptionPage', ['$scope', '$location', 'Cookie', '$cookies', function($scope, $location, Cookie, $cookies) {
+
+  var cookie = $cookies.get('github');
+  var cookieObj = Cookie.parseCookie(cookie);
+  console.log(cookieObj);
 
   var user = {
-    picture: 'assets/pictures/users/royce.jpg',
-    name: 'Royce Leung',
-    memberDate: 1447797324755,
-    ratings: Math.round(4.2),
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    projects: [
-      {name:'Ballr', id:'123'},
-      {name:'GitConnect', id:'987'},
-      {name:'Foodly', id:'567'},
-      {name:'InstaCutz', id:'0987'},
-      {name:'Humus', id:'12883'}
-    ],
+    name: cookieObj.username,
+    githubId: cookieObj.id,
+    picture: cookieObj.avatar,
     languages: ['JavaScript', 'AngularJS', 'Sass', 'CSS', 'HTML', 'Firebase']
-  }
+  };
 
   $scope.user = user;
 

99eb7094f49385f7817b82b71e971f4b6f520772
Renan Deswarte <renandeswarte@gmail.com>
2015-12-03 22:57:38 -0800

    feat: welcome page WIP

--- client/app/view/welcome/welcome.js
+++ client/app/view/welcome/welcome.js
@@ -11,4 +11,33 @@ angular.module('myApp.welcome', ['ngRoute'])
 .controller('welcomeCtrl', ['$scope', function($scope) {
   // Initiate WOW effect for the Welcome page
   new WOW().init();
+  // Initiate Stellar effect for the Welcome page only if no mobile
+  var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+  };
+    if( !isMobile.any() ){
+        $(window).stellar();
+    }
+
+  var windowHeight = $(window).height();
+
+  // Set first section height
+  $('.section.first').height(windowHeight - 51)
 }]);

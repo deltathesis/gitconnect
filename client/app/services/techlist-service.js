c2733653eccc58e414b5b59f80a32d5c9164a75e
Renan Deswarte <renandeswarte@gmail.com>
2015-12-09 15:51:23 -0800

    feat: add news languages

--- client/app/services/techlist-service.js
+++ client/app/services/techlist-service.js
@@ -2,14 +2,14 @@ angular.module('myApp.techlistService', [])
 
 .service('techList', function() {
   this.techList = [
-  "AWS", "ActionScript", "AngularJS", "Arduino", "Backbone", "Bison",
-  "Bootstrap", "Bower", "C", "C#", "C++", "CSS", "CoffeeScript", "Dart",
+  "Assembly", "AWS", "ActionScript", "AngularJS", "Arduino", "Backbone", "Bison",
+  "Bootstrap", "Bower", "C", "C#", "C++", "Clojure", "CSS", "CoffeeScript", "Dart",
   "Django", "Docker", "Drupal", "Elm", "Emacs", "Ember", "Erlang",
   "Express", "Facebook-API", "Firebase", "Foundation", "Github-API", "Go",
   "Google-API", "Grunt", "Gulp", "HTML", "Haskell", "Haxe", "Heroku",
-  "Ionic", "Java", "JavaScript", "Laravel", "Less", "LinkedIn-API", "Linux",
+  "Ionic", "Jasmine", "Java", "JavaScript", "Laravel", "Less", "LinkedIn-API", "Linux",
   "Lua", "Makefile", "Material-UI", "Meteor", "MySQL", "Neo4j", "Node.js",
-  "OCaml", "Objective-C", "PHP", "PostgreSQL", "PowerShell", "Processing",
+  "OCaml", "Objective-C", "PHP", "PostgreSQL", "PowerShell", "Processing", "Protractor",
   "Puppet", "Python", "Rails", "React", "Redis", "Ruby", "Rust", "SQLite",
   "Sass", "Scala", "Shell", "Stylus", "Swift", "Symfony", "Tcl", "TypeScript",
   "VimL", "Wordpress", "Zend", "jQuery", "mongoDB", "socket.io"

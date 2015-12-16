3de81fc8520e0b49611f84184ea855c11a797693
Royce Leung <royceleung@gmail.com>
2015-12-16 09:38:50 -0800

    bug: update passport.js

--- server/passport.js
+++ server/passport.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
 	passport.use(new GitHubStrategy({
 	    clientID: process.env.GITHUB_CLIENT_ID,
 	    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-	    callbackURL: "http://" + process.env.HOST + ":" + process.env.PORT + "/auth/github/callback"
+	    callbackURL: "http://gitconnect.me/auth/github/callback"
 	  },
 	  function(accessToken, refreshToken, profile, done) {
 	    process.nextTick(function () {
